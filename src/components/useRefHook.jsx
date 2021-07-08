ab373661351eb05e14efd3724ee3f1a686f06303
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-07-07 23:13:38 -0300

    useRef hook

---
+++ src/components/useRefHook.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+
+const UseRefHook = ( ) => {
+  // it's common to initialise refs with null
+  const inputEl = useRef( null );
+
+  const onButtonClick = () => {
+    // ⚡️ TypeScript in strict mode will complain here,
+    // because inputEl can be null!
+    inputEl.current.value = ''
+
+    inputEl.current.focus( )
+
+    if( inputEl?.current ) {
+        inputEl.current.value = ''
+       inputEl.current.focus( );
+    }
+  }
+  return (
+    <>
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>Clear and focus</button>
+    </>
+  )
+}
+
+export default UseRefHook
